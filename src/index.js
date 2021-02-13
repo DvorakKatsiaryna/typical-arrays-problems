38189edd81a5da5d6795bcfa666c8a32db1b7950
DvorakKatsiaryna <dvorak.katsiaryna@gmail.com>
2021-02-13 15:46:26 +0400

    feat: implement functions min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array && array.length > 0) {
+    return Math.min(...array);
+  }
+  return 0;  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array && array.length > 0) {
+    return Math.max(...array);
+  }
+  return 0;  
 }
 
 exports.avg = function avg (array) {
+  if (array && array.length > 0) {
+  let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+    sum = sum + array[i];  
+  }
+  let average = sum / (array.length);
+  return average;
+  }
   return 0;
 }
